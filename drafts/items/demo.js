82ba6733496900219c2411e340015711a84832b2
dustinpfister <dustin.pfister@gmail.com>
2025-04-20 09:30:53 -0400

    getItemData function started

--- drafts/items/demo.js
+++ drafts/items/demo.js
@@ -5,6 +5,7 @@ class Example extends Phaser.Scene {
     preload () {
     
         this.load.atlas('donations_16_16', '../../sheets/donations_16_16.png', '../../sheets/donations_16_16_atlas.json');
+        this.load.json('items_index', '../../items/items_index.json');
         this.load.json('household_1', '../../items/household_1.json');
         this.load.json('containers_1', '../../items/containers_1.json');
     
