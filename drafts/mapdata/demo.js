625ffe5c56255ca1acd670e6e4cd4630a8a31489
dustinpfister <dustin.pfister@gmail.com>
2025-04-24 13:59:25 -0400

    pointer down event in MapData constructor

--- drafts/mapdata/demo.js
+++ drafts/mapdata/demo.js
@@ -18,65 +18,23 @@ class Example extends Phaser.Scene {
     }
     create () {
     
-        console.log(this.cache);
-    
-        //const mapDataRaw = this.cache.tilemap.get('map1').data;
-        //const mapData = mapDataRaw.trim().replace(/\n/g, ':').split(':').map((el)=>{ return el.split(',')});
-        //const map = this.make.tilemap({ data: mapData, layers:[], tileWidth: 16, tileHeight: 16 });      
-
-        //const map = this.make.tilemap({ key: 'map1', layers:[], tileWidth: 16, tileHeight: 16 });
-
         const player = this.player = new Person( this, 40, 40, 'people_16_16', 0 );
         this.registry.set('player', player);
 
-
-        const mdc = new MapDataCollection(this, {});
-
+        const mdc = new MapDataCollection(this, { player: player });
 
         //const mapData = new MapData(this, 1, {})
         const map = mdc.mapData['map1'].map;
         const layer0 = mdc.mapData['map1'].layer0;
-        
-        this.registry.set('map', map);
-        map.setCollisionByExclusion( [ 2 ], true, true, 0 );
-        const tiles = map.addTilesetImage('map_16_16');
-        //const layer0 =  map.createLayer(0, tiles);
-        
-        
-
-        
-        //this.physics.world.setBounds(0,0, map.widthInPixels, map.heightInPixels);
-        //this.physics.add.collider( player, layer0 );
-    
-        
+                
         this.cameras.main.setZoom( 2.0 ).centerOn( player.x, player.y );
-        
-        
-        layer0.setInteractive();
-        layer0.on('pointerdown', (pointer)=>{  
-            const tx = Math.floor( pointer.worldX / 16 );
-            const ty = Math.floor( pointer.worldY / 16 );
-            const tile = map.getTileAt(tx, ty, false, 0);
-            if(tile){
-                const itemMode = player.getData('itemMode');
-                if(tile.index != 1){
-                    console.log(tile.index);
-                }
-                if(tile.index === 1 && itemMode != 2 ){    
-                    player.setPath(this, map, tx, ty);
-                }
-                if(itemMode === 2 ){
-                    //player.onHandAction(scene, null, tx, ty);
-                }
-            }
-            
-        });
+
         
     }
     update (time, delta) {
     
         const player = this.registry.get('player');
-        const map = this.registry.get('map');
+        //const map = this.registry.get('map');
         player.pathProcessorCurve(this);
         //player.offTileCheck(map);
     
