77d4b60903fd796e04a909644eeabbf9bec234fd
dustinpfister <dustin.pfister@gmail.com>
2025-06-26 17:12:00 -0400

    quick fix for items demo

--- drafts/items/demo.js
+++ drafts/items/demo.js
@@ -4,7 +4,7 @@ class Example extends Phaser.Scene {
 
     preload () {
     
-        this.load.atlas('donations_16_16', '../../sheets/donations_16_16.png', '../../sheets/donations_16_16_atlas.json');
+        this.load.atlas('donations_16_16', '../../sheets/donations_16_16.png', '../../sheets/donations_16_16.json');
         this.load.json('items_index', '../../items/items_index.json');
         this.load.json('household_1', '../../items/household_1.json');
         this.load.json('containers_1', '../../items/containers_1.json');
@@ -12,14 +12,12 @@ class Example extends Phaser.Scene {
     }
     create () {
     
-        const item = new Item(this, 'hh_mug_1', 32 * 1, 32);     
+        const item = new Item(this, 'hh_mug_1', {}, 32 * 1, 32);     
         this.add.existing(item);
         
-        const container = new Container(this, 'blue_bin', 32 * 5, 32);     
+        const container = new Container(this, 'blue_bin', {}, 32 * 5, 32);     
         this.add.existing(container);
         
-    }
-    update (time, delta) {
     }
     
 }
