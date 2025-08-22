aa33ac141f042600b11439d8392ad54635d8394c
dustinpfister <dustin.pfister@gmail.com>
2025-08-22 07:44:57 -0400

    new draw_button method to be called for each button in a menu

--- drafts/ui_menu/demo.js
+++ drafts/ui_menu/demo.js
@@ -1,4 +1,4 @@
-import { Button, GlobalControl, Menu } from '../../lib/ui/ui.js';
+import { GlobalControl, Menu } from '../../lib/ui/ui.js';
 
 class Example extends Phaser.Scene {
 
@@ -7,25 +7,7 @@ class Example extends Phaser.Scene {
         GlobalControl.setUp(this);
     
         const confMenu = Menu.createConf({
-            //bgColor: '#00af00', fgColor: '#000000',
-            colors: ['#00af00', '#000000', '#ffffff'],
-            /*
-            draw: ( ctx, texture_buttons, confMenu, scene ) => {
-                const fw = confMenu.frameWidth, fh = confMenu.frameHeight;
-                confMenu.members.forEach( ( data_button, i ) => {
-                    ctx.fillStyle = data_button.bgColor || confMenu.bgColor || '#2a2a2a';
-                    ctx.fillRect( 0, fh * i, fw, fh);
-                    ctx.fillStyle = data_button.fgColor || confMenu.fgColor || '#efefef';
-                    ctx.textBaseline = 'middle';
-                    ctx.textAlign = 'center';
-                    ctx.font = Math.floor( fh * 0.50 ) + 'px courier';
-                    const sx = 60;
-                    const x = sx + ( ( fw - sx ) * 0.95) * (i / confMenu.members.length),
-                    y = fh * i + fh / 2;
-                    ctx.fillText(data_button.desc, x, y);
-                });
-            },
-            */
+            lineWidth: 3, colors: ['#00af00', '#000000', '#ffffff'],
             members: [
                 {
                     desc: 'Foo', 
